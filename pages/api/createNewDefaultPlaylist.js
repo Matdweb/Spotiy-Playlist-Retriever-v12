28163c85fb0424086afe979597c94a78c47d6e22
mat_dweb <mat.dweb28@gmail.com>
2023-08-01 16:09:20 -0600

    new createNewDefaultPlaylists

--- pages/api/createNewDefaultPlaylist.js
+++ pages/api/createNewDefaultPlaylist.js
@@ -1,11 +1,13 @@
-import { getAccessToken } from "../../lib/spotify/getAcessToken";
+import { getAccessToken } from '../../lib/spotify/getAcessToken';
 import getUserId from '../../lib/spotify/getUserId';
 import { getSession } from "next-auth/react";
+
 
 const createNewPlaylist = async (req, res) => {
     const session = await getSession({ req });
     const refresh_token = session.token.accessToken;
-    const { id, access_token } = await getUserId(refresh_token);
+    const id = await getUserId(refresh_token);
+    const access_token = await getAccessToken(refresh_token);
     const response = await fetch(`https://api.spotify.com/v1/users/${id}/playlists`, {
         method: 'POST',
         headers: {
@@ -20,29 +22,8 @@ const createNewPlaylist = async (req, res) => {
         })
     })
     const data = await response.json();
-    console.log(data);
 
     return res.status(200).json(data);
 }
-
-
-
-// exports.createPlaylist = function(name, token) {
-//     return axios({
-//         method: 'POST',
-//         url: `https://api.spotify.com/v1/me/playlists`,
-//         data: {
-//             "name": name,
-//             "description": "Playlist generated using singlespotify by Kabir Virji",
-//             "public": true
-//         },
-//         headers: { 
-//             'Accept': 'application/json',
-//             'Content-Type': 'application/json',
-//             'Authorization': `Bearer ${token}`,
-//         }})
-//         .then(res => res.data)
-// }
-
 
 export default createNewPlaylist;
