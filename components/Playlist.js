403e589d0105e6c04a1f955e9489fcf67ee207a5
mat_dweb <mat.dweb28@gmail.com>
2023-08-08 15:11:08 -0600

    create Playlists display section

---
+++ components/Playlist.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from '../styles/components/Playlist.module.css'
+
+function Playlist({ content }) {
+    return (
+        <div key={content.id} className={styles.father}>
+            <h1 className='primary-title'>{content.name}</h1>
+            <div className={styles.container}>
+                <img className={styles.img} src={content.images[0]?.url} alt='Playlist image' />
+                <div className={styles.playlistInfo} >
+                    <label className={styles.label}>Owner</label>
+                    <p className={styles.txt}>{content.owner.display_name}</p> <br />
+                    <label className={styles.label}>Description</label>
+                    <p className={styles.txt}>{content.description}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Playlist
